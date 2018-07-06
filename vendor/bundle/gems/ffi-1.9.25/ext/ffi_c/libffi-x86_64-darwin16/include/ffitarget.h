acfc63e974acc506c1a68ca1aa94ce77da406bd8
satoshishimazaki <sattori06010601@gmail.com>
2018-07-06 19:56:44 +0900

    Bootstrapインストール

---
+++ vendor/bundle/gems/ffi-1.9.25/ext/ffi_c/libffi-x86_64-darwin16/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/satoshishimazaki/rails_app/sekou_web_app/vendor/bundle/gems/ffi-1.9.25/ext/ffi_c/libffi/src/x86/ffitarget.h
--- vendor/bundle/ruby/2.5.0/gems/ffi-1.9.25/ext/ffi_c/libffi-x86_64-darwin17/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/Users/zielson_account/sekou.com/sekou_web_app/vendor/bundle/ruby/2.5.0/gems/ffi-1.9.25/ext/ffi_c/libffi/src/x86/ffitarget.h
