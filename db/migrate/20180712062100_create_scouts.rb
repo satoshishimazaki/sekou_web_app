b5b1f28d99b3e9ebde6f0aba78ec8f2491b9cd25
satoshishimazaki <sattori06010601@gmail.com>
2018-07-12 15:48:07 +0900

    scoutsデータベース作成完了

--- db/migrate/20180712062100_create_scouts.rb
+++ db/migrate/20180712062100_create_scouts.rb
@@ -1,7 +1,7 @@
 class CreateScouts < ActiveRecord::Migration[5.2]
   def change
     create_table :scouts do |t|
-      t.integer :id
+      t.integer :agreement_count
       t.integer :client_id
       t.integer :employee_id
       t.string :summary
@@ -9,5 +9,10 @@ class CreateScouts < ActiveRecord::Migration[5.2]
 
       t.timestamps
     end
+
+    add_index :scouts ,:client_id
+    add_index :scouts ,:employee_id
+    add_index :scouts ,[:client_id,:employee_id], unique:true
   end
+
 end
