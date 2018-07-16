e789ba5ff52da496e2e238b49ab22c199a9f7f2d
satoshishimazaki <sattori06010601@gmail.com>
2018-07-16 17:27:26 +0900

    add client_company

---
+++ db/migrate/20180716082026_create_clients_companies.rb
@@ -0,0 +1,28 @@
+class CreateClientsCompanies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :clients_companies do |t|
+      t.string :name
+      t.string :address_post
+      t.integer :prefecture_id
+      t.string :address_city
+      t.string :address_building
+      t.date :established
+      t.string :president
+      t.integer :employees_number
+      t.string :employees_memo
+      t.integer :stock
+      t.string :stock_memo
+      t.string :homepage
+      t.string :time_start
+      t.string :time_end
+      t.string :time_memo
+      t.float :average_ages
+      t.string :domain_text
+      t.string :welfare
+      t.string :day_off
+      t.string :recruiter
+
+      t.timestamps
+    end
+  end
+end
