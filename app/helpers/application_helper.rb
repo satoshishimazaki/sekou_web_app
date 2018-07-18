320846aa21631ae96db419eea470dfef627a6615
satoshishimazaki <sattori06010601@gmail.com>
2018-07-18 12:13:27 +0900

    modify html and helper files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,10 +5,11 @@ module ApplicationHelper
       concat content_tag(:button, 'x' , class: 'close' , data: { dismiss: 'alert'})
       concat raw(message)
     end
+  end
 
   def parent_layout(layout)
     # @view_flow.append(:layout, self.output_buffer)
     self.output_buffer = render(file: "layouts/#{layout}")
-
   end
+
 end
