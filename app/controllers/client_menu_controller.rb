afb75f32a09dadab791bd4f7fffa4dcfa6f3301c
satoshishimazaki <sattori06010601@gmail.com>
2018-07-18 17:56:58 +0900

    add Menu file

---
+++ app/controllers/client_menu_controller.rb
@@ -0,0 +1,4 @@
+class ClientMenuController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   get 'test' => 'home#test', as: :home_test
-
+  
   devise_for :users, controllers: {
       registrations: 'users/registrations',
       sessions: 'users/sessions',
