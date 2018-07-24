c9ebb77cc15eefc5dab70636c4526edc1d119a59
satoshishimazaki <sattori06010601@gmail.com>
2018-07-24 17:52:29 +0900

    modify files

--- app/controllers/employees_accounts_controller.rb
+++ app/controllers/employees_accounts_controller.rb
@@ -1,12 +1,14 @@
 class EmployeesAccountsController < ApplicationController
 
   def index
+    @employees_accounts = EmployeesAccount
   end
 
   def edit
   end
 
   def new
+    @employees_account = Employees_account.new
   end
 
   def delete
