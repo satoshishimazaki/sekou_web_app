996d3fd41c93ce39b5fb7164adfdbf3737631473
satoshishimazaki <sattori06010601@gmail.com>
2018-07-25 16:09:32 +0900

    employee_account modify

---
+++ app/models/employee_account.rb
@@ -0,0 +1,2 @@
+class EmployeeAccount < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -94,7 +94,7 @@ ActiveRecord::Schema.define(version: 2018_07_16_084329) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "employees_accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "employee_accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name"
     t.string "first_name"
     t.string "last_name_kana"
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 2018_07_16_084329) do
   end
 
   create_table "employees_objectives", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "employee_id"
     t.integer "objectives_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
