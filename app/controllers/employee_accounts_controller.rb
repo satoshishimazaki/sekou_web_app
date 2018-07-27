d277198da45e8c1e516e0e79b8a46c8f5cd3cb55
satoshishimazaki <sattori06010601@gmail.com>
2018-07-27 14:37:04 +0900

    add createアクション & _form.html.erb at employee_accounts

--- app/controllers/employee_accounts_controller.rb
+++ app/controllers/employee_accounts_controller.rb
@@ -3,16 +3,17 @@ class EmployeeAccountsController < ApplicationController
   def index
     @employee_accounts = EmployeeAccount.all #.allが必要だと思われる
 
-    #エラーメッセージは次の通り
-    #Unable to autoload constant Employees_account, expected /Users/zielson_account/sekou.com/sekou_web_app/app/models/employees_account.rb to define it
-
   end
 
   def edit
   end
 
   def new
-    @employee_account = EmployeeAccount.new
+    @employee_accounts = EmployeeAccount.new
+  end
+
+  def create
+
   end
 
   def delete
