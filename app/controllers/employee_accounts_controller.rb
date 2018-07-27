2598d92304e05204abfcf2e74cb936e4422b9f46
satoshishimazaki <sattori06010601@gmail.com>
2018-07-27 16:45:45 +0900

    new.html.erb fix

--- app/controllers/employee_accounts_controller.rb
+++ app/controllers/employee_accounts_controller.rb
@@ -1,19 +1,17 @@
 class EmployeeAccountsController < ApplicationController
 
   def index
-    @employee_accounts = EmployeeAccount.all #.allが必要だと思われる
-
+    @employee_account = EmployeeAccount.all #.allが必要だと思われる
   end
 
   def edit
   end
 
   def new
-    @employee_accounts = EmployeeAccount.new
+    @employee_account = EmployeeAccount.new
   end
 
   def create
-
   end
 
   def delete
