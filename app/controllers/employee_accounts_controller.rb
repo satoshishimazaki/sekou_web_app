684204f067261291b05e08f2be7186733e44c38b
satoshishimazaki <sattori06010601@gmail.com>
2018-07-27 17:25:50 +0900

    employee_accouts#create made

--- app/controllers/employee_accounts_controller.rb
+++ app/controllers/employee_accounts_controller.rb
@@ -11,10 +11,41 @@ class EmployeeAccountsController < ApplicationController
     @employee_account = EmployeeAccount.new
   end
 
+  def show
+  end
+
   def create
+    @employee_account = EmployeeAccount.new(employee_account_params)
+    @employee_account.save
+    redirect_to employee_accounts_path
+  end
+
+  def employee_account_params
+    params[:employee_account].permit(
+      :employee_account_last_name,
+      :employee_account_first_name,
+      :employee_account_last_name_kana,
+      :employee_account_first_name_kana,
+      :employee_account_phone_number,
+      :employee_account_email,
+      :employee_account_birth,
+      :employee_account_sex,
+      :employee_account_employment_status,
+      :employee_account_address_post,
+      :employee_account_prefecture_id,
+      :employee_account_address_city,
+      :employee_account_address_building,
+      :employee_account_university,
+      :employee_account_faculty,
+      :employee_account_graduate,
+      :employee_account_desired_salary,
+      :employee_account_personal_summary,
+      :employee_account_not_public_company
+    )
   end
 
   def delete
   end
+
 
 end
