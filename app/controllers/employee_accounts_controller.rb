2972ada37f2d1bb1d77bdf1c59d9ba1bec660da2
satoshishimazaki <sattori06010601@gmail.com>
2018-07-30 19:29:51 +0900

    employee_accounts

--- app/controllers/employee_accounts_controller.rb
+++ app/controllers/employee_accounts_controller.rb
@@ -13,7 +13,7 @@ class EmployeeAccountsController < ApplicationController
 
   def create
     @employee_account = EmployeeAccount.new(employee_account_params)
-
+    puts(@employee_account.last_name)
     #入力内容の検証を行い、正しく入力されてればindexへ、そうでなければ、新規入力フォームを作成する。
 
     if @employee_account.save
@@ -35,28 +35,26 @@ class EmployeeAccountsController < ApplicationController
   def delete
   end
 
-  def employee_account_params
-    params[:employee_account].permit(
-      :employee_account_last_name,
-      :employee_account_first_name,
-      :employee_account_last_name_kana,
-      :employee_account_first_name_kana,
-      :employee_account_phone_number,
-      :employee_account_email,
-      :employee_account_birth,
-      :employee_account_sex,
-      :employee_account_employment_status,
-      :employee_account_address_post,
-      :employee_account_prefecture_id,
-      :employee_account_address_city,
-      :employee_account_address_building,
-      :employee_account_university,
-      :employee_account_faculty,
-      :employee_account_graduate,
-      :employee_account_desired_salary,
-      :employee_account_personal_summary,
-      :employee_account_not_public_company
-    )
-  end
+  private
+    def employee_account_params
+      params.require(:employee_account).permit(:last_name)
+#        :first_name,
+#        :last_name_kana,
+#        :first_name_kana,
+#        :phone_number,
+#        :birth,
+#        :employee_account_sex,
+#        :employee_account_employment_status,
+#        :employee_account_address_post,
+#        :employee_account_prefecture_id,
+#        :employee_account_address_city,
+#        :employee_account_address_building,
+#        :employee_account_university,
+#        :employee_account_faculty,
+#        :employee_account_graduate,
+#        :employee_account_desired_salary,
+#        :employee_account_personal_summary,
+#        :employee_account_not_public_company
+    end
 
 end
--- app/models/employee_account.rb
+++ app/models/employee_account.rb
@@ -4,36 +4,34 @@ class EmployeeAccount < ApplicationRecord
 # :employee_account_faculty, :employee_account_university,:employee_account_graduate,:employee_account_not_public_company
 # は検証対象外
 
-  validates :last_name,
-            :first_name,
-            :last_name_kana,
-            :first_name_kana,
-            :phone_number,
-            :email,
-            :birth,
-            :sex,
-            :employment_status,
-            :address_post,
-            :prefecture_id,
-            :address_city,
-            :address_building,
-            :desired_salary,
-            :personal_summary,
-  presence: true
+#  validates :last_name,
+#            :first_name,
+#            :last_name_kana,
+#            :phone_number,
+#            :email,
+#            :birth,
+#            :sex,
+#            :employment_status,
+#            :address_post,
+#            :prefecture_id,
+#            :address_city,
+#            :address_building,
+#            :desired_salary,
+#            :personal_summary,
+#  presence: true
 
-  validates :last_name,
-            :first_name,
-            :last_name_kana,
-            :first_name_kana,
-            :email,
-            :employment_status,
-            :address_post,
-            :address_city,
-            :address_building,
-  length: { maximum: 30, too_long: "最大%{count}文字まで使用できます"}
+#  validates :last_name,
+#            :first_name,
+#            :last_name_kana,
+#            :first_name_kana,
+#            :email,
+#            :employment_status,
+#            :address_city,
+#            :address_building,
+#  length: { maximum: 30, too_long: "最大%{count}文字まで使用できます"}
 
-  validates :personal_summary,
-  length: { maximum: 300, too_long: "最大%{count}文字まで使用できます"}
+#  validates :personal_summary,
+#  length: { maximum: 300, too_long: "最大%{count}文字まで使用できます"}
 
 
 
