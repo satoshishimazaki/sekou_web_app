cb0742e16a1a14b69f3c4da406b76d23c7fb4d5b
satoshishimazaki <sattori06010601@gmail.com>
2018-07-31 10:08:42 +0900

    employee_accounts fixed

--- app/controllers/employee_accounts_controller.rb
+++ app/controllers/employee_accounts_controller.rb
@@ -37,24 +37,25 @@ class EmployeeAccountsController < ApplicationController
 
   private
     def employee_account_params
-      params.require(:employee_account).permit(:last_name)
-#        :first_name,
-#        :last_name_kana,
-#        :first_name_kana,
-#        :phone_number,
-#        :birth,
-#        :employee_account_sex,
-#        :employee_account_employment_status,
-#        :employee_account_address_post,
-#        :employee_account_prefecture_id,
-#        :employee_account_address_city,
-#        :employee_account_address_building,
-#        :employee_account_university,
-#        :employee_account_faculty,
-#        :employee_account_graduate,
-#        :employee_account_desired_salary,
-#        :employee_account_personal_summary,
-#        :employee_account_not_public_company
+      params.require(:employee_account).permit(
+        :last_name,
+        :first_name,
+        :last_name_kana,
+        :first_name_kana,
+        :phone_number,
+        :birth,
+        :sex,
+        :employment_status,
+        :address_post,
+        :prefecture_id,
+        :address_city,
+        :address_building,
+        :university,
+        :faculty,
+        :graduate,
+        :desired_salary,
+        :personal_summary,
+        :not_public_company)
     end
 
 end
