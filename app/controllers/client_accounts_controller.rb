2b93b8bd4d4c155017d10d5a9ead988985a45606
NTspace <nagata.takuya.it@gmail.com>
2018-08-06 20:48:26 +0900

    client_accounts/_form.html.erb 修正

--- app/controllers/client_accounts_controller.rb
+++ app/controllers/client_accounts_controller.rb
@@ -47,6 +47,24 @@ class ClientAccountsController < ApplicationController
       params.require(:client_account).permit(
         :name,
         :address_post,
+        :prefecture_id,
+        :address_city,
+        :address_building,
+        :established,
+        :president,
+        :employees_number,
+        :employees_memo,
+        :stock,
+        :stock_memo,
+        :homepage,
+        :time_start,
+        :time_end,
+        :time_memo,
+        :average_ages,
+        :domain_text,
+        :welfare,
+        :day_off,
+        :recruiter,
         {:domain_ids => []})
     end
 end
