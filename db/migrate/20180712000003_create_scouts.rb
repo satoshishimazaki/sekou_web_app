082f4ba9bafc7f0ce36cb7818b2ba07ef7bd7292
NTspace <nagata.takuya.it@gmail.com>
2018-08-07 15:39:46 +0900

    scoutマイグレーションファイルの変更

--- db/migrate/20180712000003_create_scouts.rb
+++ db/migrate/20180712000003_create_scouts.rb
@@ -2,18 +2,12 @@ class CreateScouts < ActiveRecord::Migration[5.2]
   def change
     create_table :scouts do |t|
       t.integer :agreement_count
-      t.integer :client_id       ,foreign_key: true
-      t.integer :employee_id     ,foreign_key: true
       t.string :summary
       t.string :content
 
       t.timestamps
     end
-
-    add_index :scouts ,:client_id
-    add_index :scouts ,:employee_id
-    add_index :scouts ,[:client_id,:employee_id], unique:true
-
+    
     add_reference :scouts, :client_account, foreign_key: true
     add_reference :scouts, :employee_account, foreign_key: true
 
