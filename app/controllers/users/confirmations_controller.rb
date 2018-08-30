63c44c973529c44ecaf50464a682308119f6007a
satoshishimazaki <sattori06010601@gmail.com>
2018-08-30 18:15:27 +0900

    本登録cofirmation修正7

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -23,14 +23,15 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
    #GET /resource/confirmation?confirmation_token=abcdef
    def show
      self.resource = resource_class.confirm_by_token(params[:confirmation_token])
-     yield resource if block_given?
+     # yield resource if block_given?
 
-     if resource.errors.empty?
-
-       redirect_to home_register_fin_path
-     else
-       respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
-     end
+     redirect_to home_register_fin_path
+     # if resource.errors.empty?
+     #
+     #   redirect_to home_register_fin_path
+     # else
+     #   respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+     # end
    end
 
    protected
